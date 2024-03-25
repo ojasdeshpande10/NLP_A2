d3cd7f32c63c63d97244ec22a6786861c717f27b
ojasdeshpande10 <ojasdesh2000@gmail.com>
2024-03-25 12:09:16 -0400

    tokenize data

--- a2_p1_deshpande_115837247.py
+++ a2_p1_deshpande_115837247.py
@@ -18,11 +18,32 @@ from datasets import load_dataset
 
 
 
+def tokenize_dataset_field(dataset, tokenizer):
+    tokenized_dataset={}
+    for split in dataset:
+        for entry in dataset[split]:
+            question = tokenizer.encode(entry['question'])
+            passage =  tokenizer.encode(entry[
+            if entry['answer'] == 'True':
+                answer = 'Yes'
+            else:
+                answer = 'No'
+
+            tokenize_question = tokenizer
+
+            
+            
+
+
 
 def main():
     boolq_dataset = load_dataset('google/boolq')
     emo_dataset = load_dataset('Blablablab/SOCKET', 'emobank#valence')
-    print(boolq_dataset)
+    gpt2_tokenizer = PreTrainedTokenizerFast.from_pretrained('distilgpt2')
+    tokenize_dataset_field(boolq_dataset, gpt2_tokenizer)
+
+
+    print(boolq_dataset['train'][0])
 if __name__ == "__main__":
     main()
 
